2dbca6fef03b47e1b7e6650b382ff2a3057c00d5
viggeh2000 <79460359+VictorHylen@users.noreply.github.com>
2023-03-09 16:46:37 +0100

    connected website to firebase

--- Website/src/components/Home.js
+++ Website/src/components/Home.js
@@ -1,24 +1,71 @@
 
 import "../style/Home.css"
+import firebase from './firebase';
 
 import React, { 
     useState, 
     useEffect 
 } from 'react';
 
-import ReactSwitch from 'react-switch';
 
 
 
 function Home(){
+//Firebase
+const [Articles, setarticles] = useState([]);
+const [loading, setloading] = useState(false);
+
+  const ref = firebase.firestore().collection("Articles")
+  console.log(ref);
+
+  function getarticles(){
+    setloading(true)
+    ref.onSnapshot((querySnapshort) => {
+        const items = []
+        querySnapshort.forEach((doc) =>{
+            items.push(doc.data())
+        })
+        setarticles(items)
+        setloading(false)
+
+    })
+  }
+
+  useEffect(() =>{
+    getarticles()
+  }, [])
+
+  if(loading){
+    return <h1>Loading...</h1>
+}
+
     return (
+        <div>
         <div className="center">
-            <h1 className="shadow">Home</h1>
-    
-          
+            <h1 className="shadow">Articles</h1>
+            {Articles.map((art) => (
+        
+            <div className="border" key={art.id}>
+                
+                <h2>{art.header}</h2>
+                <h2>{art.text}</h2>
+                <h2>{art.link}</h2>
+            
+            </div>
 
-
+            ))}
+            
+        </div>        
         </div>
+
+
+        
+        
+
+
+
+        
+        
         
     );
 };
---
+++ Website/src/components/firebase.js
@@ -0,0 +1,20 @@
+import firebase from 'firebase/compat/app';
+import 'firebase/compat/auth';
+import 'firebase/compat/firestore';
+import "firebase/compat/database";
+import "firebase/compat/storage";
+
+
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = {
+    apiKey: "AIzaSyAqRIOkwrFdibY0EHKiH3UCRXPHQCM5QuU",
+    authDomain: "thesis-d3405.firebaseapp.com",
+    storageBucket: "thesis-d3405.appspot.com",
+    messagingSenderId: "657550449245",
+    appId: "1:657550449245:web:900cba698989da73674bb4",
+    measurementId: "G-4TQE6PEGL6"
+  };
+
+firebase.initializeApp(firebaseConfig)
+
+export default firebase
