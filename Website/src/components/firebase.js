5ed4a5ef3e209a10e79466750f89ae4bb817c904
viggeh2000 <79460359+VictorHylen@users.noreply.github.com>
2023-03-09 16:46:55 +0100

    ?

--- Website/src/components/firebase.js
+++ Website/src/components/firebase.js
@@ -9,6 +9,7 @@ import "firebase/compat/storage";
 const firebaseConfig = {
     apiKey: "AIzaSyAqRIOkwrFdibY0EHKiH3UCRXPHQCM5QuU",
     authDomain: "thesis-d3405.firebaseapp.com",
+    projectId: "thesis-d3405",
     storageBucket: "thesis-d3405.appspot.com",
     messagingSenderId: "657550449245",
     appId: "1:657550449245:web:900cba698989da73674bb4",
