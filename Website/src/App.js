014d079630dd71e3dd28d4a3e069f042aa03f9ac
Timmy Bergvall <79316874+TimmyBergvall@users.noreply.github.com>
2023-04-26 10:04:52 +0200

    format change only

--- Website/src/App.js
+++ Website/src/App.js
@@ -1,42 +1,43 @@
-import './App.css';
+import "./App.css";
 
-import React, { 
-  useState
-} from 'react';
+import React, { useState } from "react";
 
 import {
   BrowserRouter as Router,
   Routes,
   Route,
-  Navigate
-} from "react-router-dom"
+  Navigate,
+} from "react-router-dom";
 
-
-
-
-import Home from "./components/Home"
-import Navbar from "./components/Navbar"
-
-
+import Home from "./components/Home";
+import Navbar from "./components/Navbar";
 
 function App() {
-
   const [selectedSources, setSelectedSources] = useState([]);
-  
+
   return (
     <>
-      <Navbar selectedSources={selectedSources} setSelectedSources={setSelectedSources} />
+      <Navbar
+        selectedSources={selectedSources}
+        setSelectedSources={setSelectedSources}
+      />
       <Router>
         <Routes>
-          <Route path="/" element={<Home selectedSources={selectedSources} setSelectedSources={setSelectedSources} />} />
+          <Route
+            path="/"
+            element={
+              <Home
+                selectedSources={selectedSources}
+                setSelectedSources={setSelectedSources}
+              />
+            }
+          />
 
-
-        
           {/*If path is not found, redirect to Home*/}
           <Route path="*" element={<Navigate to="/" />} />
         </Routes>
       </Router>
-  </>
+    </>
   );
 }
 
