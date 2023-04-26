255e677f59cb4730fc216cf9b6f7f4e46aaaa9b2
Timmy Bergvall <79316874+TimmyBergvall@users.noreply.github.com>
2023-04-26 10:03:01 +0200

    Format on the code

--- Website/src/components/Home.js
+++ Website/src/components/Home.js
@@ -1,79 +1,77 @@
-import "../style/Home.css"
-import firebase from './firebase';
+import "../style/Home.css";
+import firebase from "./firebase";
 
+import React, { useState, useEffect } from "react";
 
-import React, { 
-    useState, 
-    useEffect 
-} from 'react';
+function Home({ selectedSources, setSelectedSources }) {
+  const [sources, setsources] = useState([]);
+  const [loading, setloading] = useState(false);
 
+  useEffect(() => {
+    const storedSources = JSON.parse(localStorage.getItem("selectedSources"));
+    setSelectedSources(storedSources);
+  }, []);
 
-
-function Home({selectedSources, setSelectedSources}) {
-    const [sources, setsources] = useState([]);
-    const [loading, setloading] = useState(false);
-
-    useEffect(() => {
-        const storedSources = JSON.parse(localStorage.getItem('selectedSources'));
-        setSelectedSources(storedSources);
-    }, []);
-
-    useEffect(() => {
-        localStorage.setItem('selectedSources', JSON.stringify(selectedSources));
-        const refs = selectedSources.map((source) => firebase.firestore().collection("Sources").doc(source).collection("Articles"));
-        getsources(refs);
-    }, [selectedSources]);
-
-    function getsources(refs){
-        setloading(true);
-        const items = [];
-        Promise.all(refs.map(ref => ref.get())).then((snapshots) => {
-            snapshots.forEach((snapshot) => {
-                snapshot.forEach((doc) => {
-                    items.push(doc.data());
-                });
-            });
-            setsources(items);
-            setloading(false);
-            });
-    }
-
-    const handleHeaderClick = (link) => {
-        window.open(link, "_blank");
-    };
-
-    if(loading){
-        return <h1>Loading...</h1>
-    }
-
-    if (selectedSources.length === 0) {
-        return (
-        <div className="center"> 
-        <h1>You have not selected any sources</h1>
-        <h2>Click on "Select sources" to display your alternatives and select the preferred source/sources</h2>
-        </div>
-        );
-    }
-
-    return (
-        <div>
-        <div className="center">
-            {sources.map((art) => (
-        
-            <div className="border" key={art.id}>
-                <h3 className="header" onClick={() => handleHeaderClick(art.link)}>
-                    {art.header} - {art.source}
-                </h3>
-                <p className="bodyText">{art.text}</p>
-            
-            </div>
-
-            ))}
-            
-        </div>        
-        </div>
-        
+  useEffect(() => {
+    localStorage.setItem("selectedSources", JSON.stringify(selectedSources));
+    const refs = selectedSources.map((source) =>
+      firebase
+        .firestore()
+        .collection("Sources")
+        .doc(source)
+        .collection("Articles")
     );
-};
-  
+    getsources(refs);
+  }, [selectedSources]);
+
+  function getsources(refs) {
+    setloading(true);
+    const items = [];
+    Promise.all(refs.map((ref) => ref.get())).then((snapshots) => {
+      snapshots.forEach((snapshot) => {
+        snapshot.forEach((doc) => {
+          items.push(doc.data());
+        });
+      });
+      setsources(items);
+      setloading(false);
+    });
+  }
+
+  const handleHeaderClick = (link) => {
+    window.open(link, "_blank");
+  };
+
+  if (loading) {
+    return <h1>Loading...</h1>;
+  }
+
+  if (selectedSources.length === 0) {
+    return (
+      <div className="center">
+        <h1>You have not selected any sources</h1>
+        <h2>
+          Click on "Select sources" to display your alternatives and select the
+          preferred source/sources
+        </h2>
+      </div>
+    );
+  }
+
+  return (
+    <div>
+      <div className="center">
+        {sources.map((art) => (
+          <div className="border" key={art.id}>
+            <h3 className="header" onClick={() => handleHeaderClick(art.link)}>
+              {art.header} - {art.source}
+            </h3>
+            <p className="bodyText">{art.text}</p>
+          </div>
+        ))}
+      </div>
+    </div>
+  );
+}
+
 export default Home;
--- Website/src/components/Navbar.js
+++ Website/src/components/Navbar.js
@@ -1,91 +1,91 @@
-import "../style/Navbar.css"
-import firebase from './firebase';
+import "../style/Navbar.css";
+import firebase from "./firebase";
 
-import React, { 
-    useState, 
-    useEffect 
-} from 'react';
+import React, { useState, useEffect } from "react";
 
+function Navbar({ selectedSources, setSelectedSources }) {
+  const [showSource, setShowSource] = useState(false);
+  const [sources, setSources] = useState([]);
+  const [loading, setLoading] = useState(false);
+  const [chosenSource, setChosenSource] = useState("");
+  const ref = firebase.firestore().collection("Sources");
 
-
-
-function Navbar({selectedSources, setSelectedSources}){
-    const [showSource, setShowSource] = useState(false);
-    const [sources, setSources] = useState([]);
-    const [loading, setLoading] = useState(false);
-    const [chosenSource, setChosenSource] = useState("");
-    const ref = firebase.firestore().collection("Sources");
-
-    useEffect(() => {
+  useEffect(() => {
     function getTheSources() {
-        const ref = firebase.firestore().collection("Sources");
-        ref.onSnapshot((querySnapshot) => {
-            const items = [];
-            querySnapshot.forEach((doc) =>{
-                items.push(doc.id);
-            });
-            setSelectedSources(items);
-    });
+      const ref = firebase.firestore().collection("Sources");
+      ref.onSnapshot((querySnapshot) => {
+        const items = [];
+        querySnapshot.forEach((doc) => {
+          items.push(doc.id);
+        });
+        setSelectedSources(items);
+      });
     }
     getTheSources();
-    }, []);
+  }, []);
 
-    useEffect(() => {
-        localStorage.setItem("selectedSources", JSON.stringify(selectedSources));
-    }, [selectedSources]);
-    
+  useEffect(() => {
+    localStorage.setItem("selectedSources", JSON.stringify(selectedSources));
+  }, [selectedSources]);
 
-    useEffect(() =>{
-        getSources();
-    }, []);
+  useEffect(() => {
+    getSources();
+  }, []);
 
-    const handleSourceClick = (source) => {
-        if (selectedSources.includes(source)) {
-            setSelectedSources(selectedSources.filter(s => s !== source));
-        } else {
-            setSelectedSources([...selectedSources, source]);
-        }
-    };
-    
-    function getSources(){
-        setLoading(true);
-        ref.onSnapshot((querySnapshot) => {
-            const items = [];
-            querySnapshot.forEach((doc) =>{
-                items.push(doc.id);
-            });
-            setSources(items);
-            setLoading(false);
-        });
-    }  
-
-    if(loading){
-        return <h1>Loading...</h1>
+  const handleSourceClick = (source) => {
+    if (selectedSources.includes(source)) {
+      setSelectedSources(selectedSources.filter((s) => s !== source));
+    } else {
+      setSelectedSources([...selectedSources, source]);
     }
+  };
 
-    return (
+  function getSources() {
+    setLoading(true);
+    ref.onSnapshot((querySnapshot) => {
+      const items = [];
+      querySnapshot.forEach((doc) => {
+        items.push(doc.id);
+      });
+      setSources(items);
+      setLoading(false);
+    });
+  }
+
+  if (loading) {
+    return <h1>Loading...</h1>;
+  }
+
+  return (
+    <div>
+      <div className="center">
         <div>
-            <div className="center">
-                <div>    
-                    <h2 className="shadow" onClick={() => setShowSource(!showSource)}>Select sources:</h2>
-                    {showSource && (
-                        <div>
-                            {sources.map((source) => (
-                                <div key={source}>
-                                    <h4 
-                                    className={selectedSources.includes(source) ? "selected" : ""}
-                                    onClick={() => {
-                                        handleSourceClick(source);
-                                        setChosenSource(source);
-                                    }}> {source} </h4>
-                                </div>
-                            ))}
-                        </div>  
-                    )}
-                </div>    
-            </div> 
-        </div>  
-    );
-};
+          <h2 className="shadow" onClick={() => setShowSource(!showSource)}>
+            Select sources:
+          </h2>
+          {showSource && (
+            <div>
+              {sources.map((source) => (
+                <div key={source}>
+                  <h4
+                    className={
+                      selectedSources.includes(source) ? "selected" : ""
+                    }
+                    onClick={() => {
+                      handleSourceClick(source);
+                      setChosenSource(source);
+                    }}
+                  >
+                    {" "}
+                    {source}{" "}
+                  </h4>
+                </div>
+              ))}
+            </div>
+          )}
+        </div>
+      </div>
+    </div>
+  );
+}
 export default Navbar;
-
